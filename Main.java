e6d1506effad7a991b5e0c562370cd0b62a2349f
TramVo276 <tvo941352@gmail.com>
2025-02-20 10:11:05 -0800

    tw

--- Main.java
+++ Main.java
@@ -8,6 +8,46 @@ public class Main
     Scanner sc = new Scanner(System.in);
     String input = "";
 
+    // arraylist way
+    ArrayList<Integer> foo = new ArrayList<Integer>();
+
+    foo.add(New Integer(1));
+    foo.add(New Integer(1));
+    foo.add(New Integer(1));
+
+
+    for (int i = 0; i < foo.size(); i++)
+    {
+      System.out.println(foo.get(i));
+    }
+
+    for (Integer N : foo)
+    {
+      System.out.println(N); //toString()
+      System.out.println(N.intValue()); //intValue returns the value as int
+
+    }
+
+    
+      //classic way
+    int[] monkey = {8, 9, 5, 6, 5};
+    for (int i = 0; i < monkey.length; i++)
+    {
+      System.out.println(monkey[i]); //print out every value in the list
+    
+    }
+
+    for (int n : jennys) // n can be any 
+    {
+      System.out.println(n); //print the element
+    }
+
+
+
+
+
+
+
     while (!input.equals("STOP"))
     {
       System.out.println("infinite loop! replace with your code");
@@ -17,12 +57,35 @@ public class Main
   // Problem 2 - HighestNum
   public static Integer highestNum(ArrayList<Integer> arr)
   {
-    return null;
+    ArrayList<Integer> hai = new ArrayList<Integer>();
+    int max = 
+    Integer n = new Integer(0);
+
+    for (Integer N : arr)
+    {
+      if (.. > high)
+      {
+
+      }
+    }
   }
 
   // Problem 3 - getEvens
+  // Return an ArrayList with ONLY the even numbers from arr
   public static ArrayList<Integer> getEvens(final ArrayList<Integer> arr)
   {
-    return null;
+    ArrayList<Integer> ans = new ArrayList<Integer>();
+    for (Integer N : arr)
+    {
+      if (N.intValue() % 2 == 0)
+      {
+        ans.add(N);
+      }
+    }
+
+    return ans;
+
+
+   
   }
 }
